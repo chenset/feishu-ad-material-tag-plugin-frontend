3b8602db83d3a0e9f5240a5cf8d9526a81d1f1b4
zhangjihua <zhangjihua@wimift.com>
2025-04-27 17:24:36 +0800

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -224,7 +224,7 @@ function LoadApp() {
             index: i + 1,
             time: new Date().toLocaleTimeString(),
             status: 'success',
-            message: `处理完成。${result.msg}`,
+            message: '处理完成' + (result.msg ? `: ${result.msg}` : ''),
             total: totalRecords
           }]);
         } catch (error) {
